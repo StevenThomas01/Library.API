f0dd4823c786c0ce5937e1730e3bdf72369928f9
Kiet <kietlo@gmail.com>
2019-11-21 21:37:11 +0000

    Add project files.

---
+++ Contexts/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.API.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
